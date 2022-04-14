ee9f9d0011ddadb9faade8482f2821863999bf17
Sarat Chandra <me@saratchandra.in>
2022-04-14 22:56:45 +0530

    feat: Simplifying error handling by using `anyhow`.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-mod errors;
 use std::{
     fs::{self, File},
     io,
@@ -6,8 +5,8 @@ use std::{
     process::{self, Command},
 };
 
-use clap::{Parser, Result};
-use errors::ReleasrError;
+use anyhow::{bail, Context, Result};
+use clap::Parser;
 use serde::{Deserialize, Serialize};
 
 #[derive(Parser, Debug)]
@@ -56,20 +55,14 @@ fn main() {
     }
 }
 
-fn parse_config(cfg_path: &str) -> Result<Config, ReleasrError> {
-    let cfg_str = fs::read_to_string(&cfg_path);
-    let cfg_str = match cfg_str {
-        Ok(cfg) => cfg,
-        Err(error) => {
-            println!("error reading from file {}: {}", &cfg_path, error);
-            process::exit(1);
-        }
-    };
+fn parse_config(cfg_path: &str) -> Result<Config> {
+    let cfg_str = fs::read_to_string(&cfg_path)
+        .with_context(|| format!("error reading config file at {}", cfg_path))?;
     let cfg: Config = serde_yaml::from_str(&cfg_str)?;
     Ok(cfg)
 }
 
-fn run(cfg: &Config) -> Result<(), ReleasrError> {
+fn run(cfg: &Config) -> Result<()> {
     println!("builds: {:?}", cfg.builds);
     for build in &cfg.builds {
         println!("executing build \"{}\"", build.name);
@@ -80,7 +73,7 @@ fn run(cfg: &Config) -> Result<(), ReleasrError> {
     Ok(())
 }
 
-fn run_job(build: &Build, job: &Job) -> Result<(), ReleasrError> {
+fn run_job(build: &Build, job: &Job) -> Result<()> {
     // Split cmd into command, args.
     let cmds = job.command.split(" ").collect::<Vec<&str>>();
     let output = Command::new(cmds[0]).args(&cmds[1..]).output()?;
@@ -92,28 +85,28 @@ fn run_job(build: &Build, job: &Job) -> Result<(), ReleasrError> {
         fs::copy(
             &job.artifact,
             Path::new(&build.dist_folder).join(&job.bin_name),
-        )?;
+        )
+        .with_context(|| format!("error while copying artifact: {}", job.artifact))?;
 
         let dist_folder = Path::new(&build.dist_folder).join(&job.bin_name);
         let bin_path = dist_folder.to_str();
         let bin_path = match bin_path {
             None => {
-                return Err(ReleasrError::CustomError(String::from(
-                    "error creating bin path",
-                )))
+                bail!("error creating bin path");
             }
             Some(bin_path) => bin_path,
         };
 
         // Create an archive.
         println!("creating an archive for {}", &job.name);
-        archive_file(bin_path, &build.dist_folder, &job.name)?;
+        archive_file(bin_path, &build.dist_folder, &job.name)
+            .with_context(|| format!("error while creating archive for job: {}", job.name))?;
     }
 
     Ok(())
 }
 
-fn archive_file(filename: &str, dist: &str, name: &str) -> Result<(), ReleasrError> {
+fn archive_file(filename: &str, dist: &str, name: &str) -> Result<()> {
     let mut f = File::open(filename)?;
     // Create a zip file.
     let mut zip_path = Path::new(&dist).join(name);
@@ -122,7 +115,7 @@ fn archive_file(filename: &str, dist: &str, name: &str) -> Result<(), ReleasrErr
     let mut zip = zip::ZipWriter::new(&mut zip_file);
 
     let options =
-        zip::write::FileOptions::default().compression_method(zip::CompressionMethod::Stored);
+        zip::write::FileOptions::default().compression_method(zip::CompressionMethod::Bzip2);
     zip.start_file(filename, options)?;
     io::copy(&mut f, &mut zip)?;
 
