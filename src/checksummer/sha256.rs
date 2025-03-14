6d2c5d39aecda41c69a9183b129b0b56b7172e52
Sarat Chandra <me@saratchandra.in>
2025-03-14 12:28:53 +0530

    refactor: Increase buffer size and also improve error handling.

--- src/checksummer/sha256.rs
+++ src/checksummer/sha256.rs
@@ -1,25 +1,32 @@
 use crate::checksummer::Checksummer;
 use async_trait::async_trait;
-use color_eyre::eyre::Result;
+use color_eyre::eyre::{Result, WrapErr};
 use sha2::Digest;
 use tokio::fs::File;
 use tokio::io::AsyncReadExt;
 
 pub struct Sha256 {}
 
-#[allow(clippy::needless_arbitrary_self_type)]
 #[async_trait]
 impl Checksummer for Sha256 {
-    async fn compute(self: &Self, file_path: &str) -> Result<String> {
-        let mut file = File::open(file_path).await?;
-        let mut buffer = vec![0; 4096];
+    async fn compute(&self, file_path: &str) -> Result<String> {
+        let mut file = File::open(file_path)
+            .await
+            .wrap_err_with(|| format!("Failed to open file: {}", file_path))?;
+
         let mut hasher = sha2::Sha256::new();
+        let mut buffer = vec![0; 16 * 1024]; // 16KB buffer for better performance
 
         loop {
-            let bytes_read = file.read(&mut buffer).await?;
+            let bytes_read = file
+                .read(&mut buffer)
+                .await
+                .wrap_err_with(|| format!("Failed to read from file: {}", file_path))?;
+
             if bytes_read == 0 {
                 break;
             }
+
             hasher.update(&buffer[..bytes_read]);
         }
 
